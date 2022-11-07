a7b19237b0f560e21b112b50fbcfbea2c450c8ce
Franciscobcr <114684010+Franciscobcr@users.noreply.github.com>
2022-11-07 13:24:08 -0300

    Create comissão.c

---
+++ comissão.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+   
+    float fixo,vendas, comissao, total;
+    
+    printf("Digite o salÃ¡rio do vendedor: ");
+    scanf("%f",&fixo);
+    printf("\nDigite o valor total das vendas: ");
+    scanf("%f",&vendas);
+    
+    if(vendas<=1200){
+        comissao=0.03*vendas;
+    }
+    else{
+        comissao=0.05*vendas;
+    }
+    
+    total=fixo+comissao;
+    
+    printf("O salario total do vendedor Ã© %.2f",total);
+    
+    return 0;
+}
